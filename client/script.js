56600184cb454936ceff69ec85bb141a0cf4a768
Jonah Ebent <jonahaaronebent@gmail.com>
2025-02-04 13:36:19 -0500

    Add WebSocket thruster control and connection status display

--- client/script.js
+++ client/script.js
@@ -2,11 +2,29 @@
 // and connect to the server
 const socket = new WebSocket("ws://localhost:8080");
 
+window.onload = function () {
+  var space_bar = 32;
+
+  window.onkeydown = function (key) {
+    if (key.keyCode === space_bar) {
+      socket.send('thruster on');
+    };
+  };
+
+  window.onkeyup = function (key) {
+    if (key.keyCode === space_bar) {
+      socket.send('thruster off');
+    };
+  }
+};
+
+
 // Event listener for when
 //the WebSocket connection is opened
 socket.onopen = function (event) {
   // Alert the user that they are
   // connected to the WebSocket server
+  document.getElementById("status").innerHTML = "<p>connected</p>";
   alert("You are Connected to WebSocket Server");
 };
 
@@ -17,7 +35,7 @@ socket.onmessage = function (event) {
   const outputDiv = document.getElementById("output");
   // Append a paragraph with the
   //  received message to the output div
-  outputDiv.innerHTML += `<p>Received <b>"${event.data}"</b> from server.</p>`;
+  outputDiv.innerHTML = `<p>Received <b>"${event.data}"</b> from server.</p>`;
 };
 
 // Event listener for when the
@@ -25,20 +43,6 @@ socket.onmessage = function (event) {
 socket.onclose = function (event) {
   // Log a message when disconnected
   //  from the WebSocket server
+  document.getElementById("status").innerHTML = "<p>not connected</p>";
   console.log("Disconnected from WebSocket server");
 };
-
-// Function to send a message
-//  to the WebSocket server
-function sendMessage() {
-  // Get the message input element
-  const messageInput = document.getElementById("messageInput");
-  // Get the value of
-  // the message input
-  const message = messageInput.value;
-  // Send the message to
-  // the WebSocket server
-  socket.send(message);
-  // Clear the message input
-  messageInput.value = "";
-}
--- server/index.js
+++ server/index.js
@@ -6,8 +6,16 @@ wss.on('connection', function connection(ws) {
   console.log('Client connected');
 
   ws.on('message', function incoming(message) {
-      console.log('Received: %s', message);
-      ws.send(`${message}`);
+    if (message == 'thruster on') {
+      console.log('Thruster is on 🔥');
+      ws.send('Thruster is on 🔥');
+    } else if (message == 'thruster off') {
+      console.log('Thruster is off 💨')
+      ws.send('Thruster is off 💨');
+    } else {
+      console.log(`Unknown message: ${message}`);
+      ws.send(`Unknown message: ${message}`);
+    }
   });
 
   ws.on('close', function () {
