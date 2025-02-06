d62695bd05e311148fdf09dce013b731c43d5c6b
ah-OOG-ah <75745146+ah-OOG-ah@users.noreply.github.com>
2025-02-05 22:07:19 -0500

    Fix physics

--- server/index.js
+++ server/index.js
@@ -1,24 +1,23 @@
 const NS_PER_MS = 1_000_000;
 const MS_PER_TICK = 50;
+
+const sleep = ms => new Promise(r => setTimeout(r, ms));
 const WebSocket = require('ws');
 
 const wss = new WebSocket.Server({ port: 8080 });
 
 wss.on('connection', async function connection(ws) {
   console.log('Client connected');
-  var isBurning = false;
+  var isBurning = true;
+  var isDead = false;
 
   // Register events
   ws.on('message', function incoming(message) {
     if (message == 'thruster on') {
       isBurning = true;
-
-      console.log('Thruster is on 🔥');
       ws.send('Thruster is on 🔥');
     } else if (message == 'thruster off') {
       isBurning = false;
-
-      console.log('Thruster is off 💨')
       ws.send('Thruster is off 💨');
     } else {
       console.log(`Unknown message: ${message}`);
@@ -28,21 +27,29 @@ wss.on('connection', async function connection(ws) {
 
   ws.on('close', function () {
       console.log('Client disconnected');
+      isDead = true;
   });
 
   // Now that there's a connection, start the server
-  var blackboard = { hey: "notnull!" };
-  while (ws.OPEN) {
+  var blackboard = new Map();
+  console.log(blackboard);
+  while (true) {
     // Tick start
     var time = process.hrtime.bigint();
 
     // TODO: Run modules
-    physicsMod.apply(blackboard);
+    controlsMod(blackboard, isBurning);
+    physicsMod(blackboard);
+    loggingMod(blackboard);
 
     // Wait for next tick
-    var time = (process.hrtime.bigint() - time) / NS_PER_MS;
-    if (time > MS_PER_TICK) { console.log("Behind %i ms, skipping %i ticks", time, time / MS_PER_TICK); }
-    else { await sleep(time); }
+    var elapsed = Number(process.hrtime.bigint() - time)
+    elapsed = elapsed / NS_PER_MS;
+    if (elapsed > MS_PER_TICK) { console.log("Behind %i ms, skipping %i ticks", elapsed, elapsed / MS_PER_TICK); }
+    else { 
+      await sleep(MS_PER_TICK - elapsed);
+      if (isDead) { break; }
+    }
   }
 });
 
@@ -67,45 +74,45 @@ wss.on('connection', async function connection(ws) {
  * m = F_thrust / g_0 / I_sp
  * Dimensional analysis: kg/s = (kg m/s^2) / (m/s^2) / s
  */
-var physicsMod = function(blackboard) {
+function physicsMod(blackboard) {
   const TIME_STEP = MS_PER_TICK / 1_000;
   const G_0 = 9.80665;
-  const LUNAR_MASS = 7.346 * 10e22;
+  const LUNAR_MASS = 7.346 * 10**22;
   const LUNAR_RADIUS = 1_737_400;
-  const G = 6.6743 * 10e-11;
+  const G = 6.6743 * 10**-11;
 
   const I_SP = 311;
   const THRUST = 45_040;
   const MASS_FLOW = THRUST / G_0 / I_SP;
 
-  if (Object.hasOwn(blackboard, "position")) { blackboard.position = 15_000; }
-  if (Object.hasOwn(blackboard, "velocity")) { blackboard.velocity = 0; }
-  if (Object.hasOwn(blackboard, "mass")) { blackboard.mass = 16_400; }
+  if (!blackboard.get("position")) { blackboard.set("position", 15_000 + LUNAR_RADIUS); }
+  if (!blackboard.get("velocity")) { blackboard.set("velocity", 0); }
+  if (!blackboard.get("mass")) { blackboard.set("mass", 16_400); }
 
-  let lunarG = G * LUNAR_MASS / (blackboard.position ** 2);
-  var acceleration = lunarG;
+  let lunarG = G * LUNAR_MASS / (blackboard.get("position")**2);
+  var acceleration = -lunarG;
 
-  if (blackboard.isBurning) {
-    acceleration += THRUST / blackboard.mass;
-    blackboard.mass -= MASS_FLOW * TIME_STEP;
+  if (blackboard.get("isBurning")) {
+    acceleration += THRUST / blackboard.get("mass");
+    blackboard.set("mass", blackboard.get("mass") - MASS_FLOW * TIME_STEP);
   }
 
   // Update position and velocity
-  blackboard.position += blackboard.velocity * TIME_STEP;
-  blackboard.velocity += acceleration * TIME_STEP;
+  blackboard.set("position", blackboard.get("position") + blackboard.get("velocity") * TIME_STEP);
+  blackboard.set("velocity", blackboard.get("velocity") + acceleration * TIME_STEP);
 }
 
 /**
  * Simply updates the blackboard with the given variable. The split ensures
  * that isBurning only gets updated once per tick.
  */
-var controlsMod = function(blackboard, isBurning) {
-  blackboard.isBurning = isBurning;
+function controlsMod(blackboard, isBurning) {
+  blackboard.set("isBurning", isBurning);
 }
 
 /**
  * Dumps the blackboard to the log
  */
-var loggingMod = function(blackboard) {
+function loggingMod(blackboard) {
   console.log(blackboard);
 }
