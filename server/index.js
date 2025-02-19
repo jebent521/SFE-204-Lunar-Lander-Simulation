490133ddc966691dbb899a2d300ac3f2074b8083
ah-OOG-ah <75745146+ah-OOG-ah@users.noreply.github.com>
2025-02-19 07:49:35 -0500

    Log even when paused

--- server/index.js
+++ server/index.js
@@ -92,6 +92,7 @@ wss.on('connection', async function connection(ws) {
       if (holder.disconnected) { break; }
     }
   }
+
   ws.send(JSON.stringify({stats: statisticsMod.getCurrentStats(blackboard)}));
 });
 
