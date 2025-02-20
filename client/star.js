f5c441102ad4d5f82b80e3771a495ae702c7f335
Evan Whitmer <whitmercraft939@gmail.com>
2025-02-20 09:34:55 -0500

    no point are stars not on screen

--- client/star.js
+++ client/star.js
@@ -1,16 +1,16 @@
 
 function generateBoxShadows(numShadows, xOffsetRange, yOffsetRange, color) {
-    const boxShadows = [];
-    for (let i = 0; i < numShadows; i++) {
-      const xOffset = Math.floor(Math.random() * (xOffsetRange[1] - xOffsetRange[0] + 1)) + xOffsetRange[0];
-      const yOffset = Math.floor(Math.random() * (yOffsetRange[1] - yOffsetRange[0] + 1)) + yOffsetRange[0];
-      
-      const boxShadow = `${xOffset}px ${yOffset}px ${color}`;
-      boxShadows.push(boxShadow);
-    }
-    return boxShadows;
+  const boxShadows = [];
+  for (let i = 0; i < numShadows; i++) {
+    const xOffset = Math.floor(Math.random() * (xOffsetRange[1] - xOffsetRange[0] + 1)) + xOffsetRange[0];
+    const yOffset = Math.floor(Math.random() * (yOffsetRange[1] - yOffsetRange[0] + 1)) + yOffsetRange[0];
+
+    const boxShadow = `${xOffset}px ${yOffset}px ${color}`;
+    boxShadows.push(boxShadow);
   }
-  
+  return boxShadows;
+}
+
 // Example usage:
 const numShadows = 700;
 const xOffsetRange = [0, 2000];
@@ -28,7 +28,7 @@ stars.style.height = "1px";
 stars.style.background = "transparent";
 stars.style.borderRadius = '50%';
 stars.style.boxShadow = `${boxShadowList}`;
-stars.style.animation = 'animStar 10s linear infinite';
+stars.style.animation = 'animStar 40s linear infinite';
 
 const style = document.createElement('style');
 document.head.appendChild(style);
@@ -45,7 +45,7 @@ stars2.style.height = "2px";
 stars2.style.background = "transparent";
 stars2.style.borderRadius = '50%';
 stars2.style.boxShadow = `${boxShadowList2}`;
-stars2.style.animation = 'animStar 20s linear infinite';
+stars2.style.animation = 'animStar 80s linear infinite';
 
 const numShadows3 = 100;
 const boxShadowList3 = generateBoxShadows(numShadows3, xOffsetRange, yOffsetRange, color);
@@ -56,7 +56,7 @@ stars3.style.height = "3px";
 stars3.style.background = "transparent";
 stars3.style.borderRadius = '50%';
 stars3.style.boxShadow = `${boxShadowList3}`;
-stars3.style.animation = 'animStar 20s linear infinite';
+stars3.style.animation = 'animStar 120s linear infinite';
 
 const stars1clone = stars.cloneNode();
 stars1clone.style.animationDelay = "-5s";
