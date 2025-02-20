d1d28c973b94568066a66e977f2508302ed5d845
ah-OOG-ah <75745146+ah-OOG-ah@users.noreply.github.com>
2025-02-20 11:38:03 -0500

    Add server states

--- server/index.js
+++ server/index.js
@@ -1,3 +1,7 @@
+// npm modules
+const WebSocket = require('ws');
+
+// Custom modules
 const statisticsMod = require('./modules/statistics');
 const controlsMod = require('./modules/controls');
 const loggingMod = require('./modules/logging');
@@ -5,26 +9,34 @@ const communicationMod = require('./modules/communications');
 
 const TIME_ACCELERATION = 1;
 
+// Time constants
 const NS_PER_MS = 1_000_000;
 const MS_PER_TICK = 50;
-
 const TIME_STEP = MS_PER_TICK / 1_000;
+
+// Physical constants
 const G_0 = 9.80665;
 const LUNAR_MASS = 7.346 * 10**22;
 const LUNAR_RADIUS = 1_737_400;
 const G = 6.6743 * 10**-11;
 
+// Lander specs
+const INVALID_MASS = -1;
 const FUEL_MASS = 8_200;
 const DRY_MASS = 8_200;
 const I_SP = 311;
 const THRUST = 45_040;
 const MASS_FLOW = THRUST / G_0 / I_SP;
-
 const WARN_VEL = -3;
 const KILL_VEL = -5;
 
+// Game states
+const MENU = "menu";
+const PAUSED = "paused";
+const PLAYING = "playing";
+const GAME_END = "end";
+
 const sleep = ms => new Promise(r => setTimeout(r, ms));
-const WebSocket = require('ws');
 
 const wss = new WebSocket.Server({ port: 8080 });
 
@@ -35,9 +47,11 @@ wss.on('connection', async function connection(ws) {
   var blackboard = {
     position: 15_000 + LUNAR_RADIUS,
     velocity: 0,
-    mass: FUEL_MASS + DRY_MASS,
+    fuel_mass: FUEL_MASS,
+    dry_mass: DRY_MASS,
     isBurning: false,
-    health: 100
+    health: 100,
+    state: "menu"
   };
 
   // Holding variables. These may change many times per tick, 
@@ -45,7 +59,10 @@ wss.on('connection', async function connection(ws) {
   var holder = {
     isBurning: false,
     disconnected: false,
-    isPaused: false
+    isPaused: false,
+    fuel_mass: INVALID_MASS,
+    dry_mass: INVALID_MASS,
+    restart: false
   };
 
   // Register events
@@ -68,13 +85,51 @@ wss.on('connection', async function connection(ws) {
   });
 
   // Now that there's a connection, start the server
-  console.log(blackboard);
   statisticsMod.addAttempt(blackboard);
   while (true) {
     // Tick start
     var time = process.hrtime.bigint();
 
-    if (!holder.isPaused) {
+    // Process state changes
+    // If in the menu, move to playing and reset the blackboard once the weight has been recieved
+    // If playing, pause at will
+    // If paused, unpause at will
+    // If game ended, switch to menu on request and if so, reset the holder
+    switch (blackboard.state) {
+      case MENU:
+        // When ready, start the game and reset the blackboard
+        if (holder.fuel_mass > 0 && holder.dry_mass > 0) { 
+          blackboard.position = 15_000 + LUNAR_RADIUS;
+          blackboard.velocity = 0;
+          blackboard.fuel_mass = holder.fuel_mass;
+          blackboard.dry_mass = holder.dry_mass;
+          blackboard.isBurning = false;
+          blackboard.health = 100;
+          blackboard.state = PLAYING;
+
+          holder.isPaused = false;
+        }
+        break;
+      case PLAYING:
+        if (holder.isPaused) { blackboard.state = PAUSED; }
+        break;
+      case PAUSED:
+        if (!holder.isPaused) { blackboard.state = PLAYING; }
+        break;
+      case GAME_END:
+        if (holder.restart) { 
+          blackboard.state = MENU;
+
+          holder.isBurning = false;
+          holder.isPaused = false;
+          holder.fuel_mass = INVALID_MASS;
+          holder.dry_mass = INVALID_MASS;
+          holder.restart = false;
+        }
+        break;
+    }
+
+    if (blackboard.state == PLAYING) {
       controlsMod(blackboard, holder.isBurning);
       physicsMod(blackboard);
       statisticsMod.recordHighestAltitude(blackboard);
@@ -92,8 +147,6 @@ wss.on('connection', async function connection(ws) {
       if (holder.disconnected) { break; }
     }
   }
-
-  ws.send(JSON.stringify({stats: statisticsMod.getCurrentStats(blackboard)}));
 });
 
 /**
@@ -120,20 +173,20 @@ wss.on('connection', async function connection(ws) {
 function physicsMod(blackboard) {
   var position = blackboard.position;
   var velocity = blackboard.velocity;
-  var mass = blackboard.mass;
+  var fuel = blackboard.fuel_mass;
   var isBurning = blackboard.isBurning;
 
   let lunarG = G * LUNAR_MASS / (position**2);
   var acceleration = -lunarG;
 
   if (isBurning) {
-    mass -= MASS_FLOW * TIME_STEP;
+    fuel -= MASS_FLOW * TIME_STEP;
 
-    if (mass < DRY_MASS) {
-      mass = DRY_MASS;
+    if (fuel < 0) {
+      fuel = 0;
       isBurning = false;
     } else {
-      acceleration += THRUST / mass;
+      acceleration += THRUST / (fuel + blackboard.dry_mass);
     }
   }
 
@@ -145,21 +198,22 @@ function physicsMod(blackboard) {
 
   if (altitude <= 0){
     if (velocity < KILL_VEL) {
+
       blackboard.health = 0;
-      isDead = true;
       statisticsMod.addCrash(blackboard);
-    }
-    else if (velocity < WARN_VEL) {
+    } else if (velocity < WARN_VEL) {
+
       blackboard.health = 100 - (velocity - WARN_VEL / (KILL_VEL - WARN_VEL) * 100);
-      // FIXME: completely safe landings don't register
-      statisticsMod.addLanding(blackboard);
     }
 
+    blackboard.state = GAME_END;
     position = LUNAR_RADIUS; velocity = 0; altitude = 0;
+    statisticsMod.addLanding(blackboard);
+    ws.send(JSON.stringify({stats: statisticsMod.getCurrentStats(blackboard)}));
   }
 
   blackboard.isBurning = isBurning;
-  blackboard.mass = mass;
+  blackboard.fuel_mass = fuel;
   blackboard.position = position;
   blackboard.altitude = altitude;
   blackboard.velocity = velocity;
